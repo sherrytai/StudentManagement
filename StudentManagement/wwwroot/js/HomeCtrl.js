c00c2ca5d71732af0ad1f1f15ac287d9b3e62ff9
Sherry <sherrytai88@gmail.com>
2019-11-17 21:49:32 -0800

    Homepage

---
+++ StudentManagement/wwwroot/js/HomeCtrl.js
@@ -0,0 +1,24 @@
+﻿'use strict'
+
+var app=angular.module("app")
+app.controller("HomeCtrl", function($scope, $http, $route){
+
+		$scope.$route = $route;
+
+
+		$http({
+		            method : "GET",
+		            url : "api/v1/products"
+		        }).then(function mySuccess(response) {
+
+		            $scope.products = response.data;
+		            console.log(response.data);
+
+		        }, function myError(response) {
+
+	                    console.log(response);
+	                    alert(response.data.message);
+
+		        });
+
+});
